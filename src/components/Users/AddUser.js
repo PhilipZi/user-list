9c508ec9fdbbb2d4430455e5be136033307920b2
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-02-27 20:06:12 +0100

    add error function and uplift

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -50,10 +50,13 @@ function addUserHandler(event){
     setEnteredUserAge('')
     }
     
+    function errorHandler(){
+        setError(null)
+    }
 
     return (
         <div>
-        {error && <ErrorModal title={error.title} message={error.message}/>}
+        {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
         <Card className={classes.input}>
             <form  onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
