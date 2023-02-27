a9f93ee43c3ca26c3e4ff2908fcdf654b63e6df1
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-02-27 20:00:43 +0100

    add ternary function to modal

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -13,6 +13,7 @@ function AddUser(props) {
 
 const [enteredUserName, setEntereduserName] = useState('')
 const [enteredUserAge, setEnteredUserAge] = useState('')
+const [error, setError] = useState();
 
 function nameChangeHandler(event) {
     setEntereduserName(event.target.value)
@@ -27,9 +28,17 @@ function addUserHandler(event){
     event.preventDefault()
 
     if(enteredUserName.trim().length === 0 || enteredUserAge.trim().length === 0 ) {
+        setError({
+            title: 'invalid input',
+            message: 'Please enter a valid name and age (non-empty values).'
+        })
         return; 
     }
     if (+enteredUserAge < 1) {
+        setError({
+            title: 'Invalid age',
+            message: 'Please enter a valid age (> 0).'
+        })
     return;
     }
     
@@ -44,13 +53,13 @@ function addUserHandler(event){
 
     return (
         <div>
-        <ErrorModal title="An error occured!" message="Something went wrong!"/>
+        {error && <ErrorModal title={error.title} message={error.message}/>}
         <Card className={classes.input}>
             <form  onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
                 <input id="username" type="text" value={enteredUserName} onChange={nameChangeHandler}/>
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age"type="number" value={enteredUserAge} min="1" step="1" onChange={ageChangeHandler}/>
+                <input id="age"type="number" value={enteredUserAge} onChange={ageChangeHandler}/>
                 <Button type="submit">Add User</Button>
                 
             </form>
